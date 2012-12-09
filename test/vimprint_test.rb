27861ad8156ac3714bfee017c80c5cfcefd34cac
Drew Neil <andrew.jr.neil@gmail.com>
2012-12-09 12:42:10 +0000

    Switch from tests to specs

--- test/vimprint_test.rb
+++ test/vimprint_test.rb
@@ -1,9 +1,8 @@
 require 'minitest/autorun'
 require 'vimprint'
 
-class Test < MiniTest::Unit::TestCase
-  def test_parse
-    result = Vimprint.parse("IHello, World!\e")
-    assert_equal "I{Hello, World!}", result
+describe Vimprint do
+  it "parses input and transforms into output" do
+    Vimprint.parse("IHello, World!\e").must_equal "I{Hello, World!}"
   end
 end
