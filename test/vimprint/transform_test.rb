07e128402f5c8dab218d2a42a38764fd8731d66a
Mislav & Drew Neil <andrew.jr.neil@gmail.com>
2012-12-10 18:30:34 +0100

    add Transform mapping tests

---
+++ test/vimprint/transform_test.rb
@@ -0,0 +1,42 @@
+require "minitest/autorun"
+require "vimprint"
+
+describe Vimprint::Transform do
+
+  def transform tree
+    transformer = Vimprint::Transform.new
+    transformer.apply tree
+  end
+
+  def assert_operation expected_name, obj
+    got_name = obj.class.name.split("::").last.intern
+    assert_equal expected_name, got_name
+  end
+
+  MiniTest::Expectations.module_eval do
+    infect_an_assertion :assert_operation, :must_be_operation
+  end
+
+  it "is a no-op for empty tree" do
+    transform([]).must_equal []
+  end
+
+  it "generates an ex command" do
+    cmd = {
+      :prompt => ':',
+      :typing => 'cq',
+      :enter  => "\r",
+    }
+    transform(cmd).must_be_operation :ExCommand
+  end
+
+  it "generates an insertion" do
+    cmd = {
+      :switch => 'i',
+      :typing => 'Hello, World!',
+      :escape => "\e",
+    }
+    transform(cmd).must_be_operation :Insertion
+  end
+
+end
