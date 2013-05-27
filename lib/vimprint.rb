9131f6000ae8fde68c730f840846c6c53150abae
Drew Neil <andrew.jr.neil@gmail.com>
2013-05-27 16:44:44 +0100

    Wire up the bin/vimprint command with new parser

--- lib/vimprint.rb
+++ lib/vimprint.rb
@@ -1,15 +1,11 @@
 require "vimprint/version"
 require "vimprint/parser"
-require "vimprint/transform"
-require "vimprint/html_presenter"
-require "nokogiri"
 
 module Vimprint
   def self.parse(input)
-    parser = Parser.new
-    transformer = Transform.new
-
-    tree = parser.parse(input)
-    transformer.apply(tree).map {|o| o.to_s }
+    normal = NormalMode.new
+    parser = VimParser.new(EventList.new(normal))
+    parser.process(input)
+    normal.eval
   end
 end
