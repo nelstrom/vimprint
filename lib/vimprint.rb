ad7ca23704d14abd0bfebedaf967edb6e770ef54
Drew Neil <andrew.jr.neil@gmail.com>
2013-05-27 16:49:08 +0100

    rename parser.rb back to vim_parser.rb

--- lib/vimprint.rb
+++ lib/vimprint.rb
@@ -1,11 +1,11 @@
 require "vimprint/version"
-require "vimprint/parser"
+require "vimprint/vim_parser"
 
 module Vimprint
   def self.parse(input)
     normal = NormalMode.new
     parser = VimParser.new(EventList.new(normal))
     parser.process(input)
-    normal.eval
+    normal.eval.map { |o| o.inspect }
   end
 end
