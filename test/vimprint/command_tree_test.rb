f60100eb31a2703371e1a9e53d19d2561468deca
Drew Neil <andrew.jr.neil@gmail.com>
2013-05-31 19:23:57 +0100

    Add a ModeCloser test with 2-deep modestack

--- test/vimprint/command_tree_test.rb
+++ test/vimprint/command_tree_test.rb
@@ -42,20 +42,29 @@ module Vimprint
       end
 
       it 'pushes a new mode when receiving a ModeOpener' do
-        a = ['a'].extend Vimprint::ModeOpener
-        b = ['b'].extend Vimprint::ModeOpener
-        @tree << a
-        @tree << b
+        @tree.<<(['a'].extend Vimprint::ModeOpener)
+        @tree.<<(['b'].extend Vimprint::ModeOpener)
         @tree.root.must_equal [['a',['b']]]
       end
 
-      it 'pops the top-most entry_point when receiving a ModeCloser' do
+      it 'pops the modestack when receiving a ModeCloser' do
         @tree.<<('a')
         @tree.push_mode
         @tree.<<('b')
         @tree.<<('c'.extend Vimprint::ModeCloser)
         @tree.<<('d')
         @tree.root.must_equal ['a',['b', 'c'], 'd']
+      end
+
+      it 'pops the modestack for each ModeCloser received' do
+        @tree.<<('a')
+        @tree.push_mode
+        @tree.<<('b')
+        @tree.push_mode
+        @tree.<<('c'.extend Vimprint::ModeCloser)
+        @tree.<<('d')
+        @tree.<<('e')
+        @tree.root.must_equal ['a',['b', ['c'], 'd'], 'e']
       end
 
       it 'appends ModalCommands to the current entry_point'
