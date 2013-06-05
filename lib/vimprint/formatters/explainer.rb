8d71b4744e98517bbda63aca8187b7a58582967e
Drew Neil <andrew.jr.neil@gmail.com>
2013-06-05 18:17:06 +0100

    Reformat code for readability

--- lib/vimprint/formatters/explainer.rb
+++ lib/vimprint/formatters/explainer.rb
@@ -19,24 +19,67 @@ module Vimprint
       {
         :normal => {
           # motions
-          'h' => ["move left 1 character","move left #{count} characters"],
-          'j' => ['move down 1 line', "move down #{count} lines"],
-          'k' => ["move up 1 line", "move up #{count} lines"],
-          'l' => ["move right 1 character", "move right #{count} characters"],
-          'b' => ["move to start of current/previous word","move to start of current/previous word #{count} times"],
-          'w' => ["move to start of next word","move to start of next word #{count} times"],
-          'e' => ["move to end of current/next word","move to end of current/next word #{count} times"],
+          'h' => [
+            "move left 1 character",
+            "move left #{count} characters"
+          ],
+          'j' => [
+            "move down 1 line",
+            "move down #{count} lines"
+          ],
+          'k' => [
+            "move up 1 line",
+            "move up #{count} lines"
+          ],
+          'l' => [
+            "move right 1 character",
+            "move right #{count} characters"
+          ],
+          'b' => [
+            "move to start of current/previous word",
+            "move to start of current/previous word #{count} times"
+          ],
+          'w' => [
+            "move to start of next word",
+            "move to start of next word #{count} times"
+          ],
+          'e' => [
+            "move to end of current/next word",
+            "move to end of current/next word #{count} times"
+          ],
           '0' => ["move to start of current line"],
           # switches
-          'i' => ["insert in front of cursor","insert #{count} times in front of cursor"],
-          'I' => ["insert at start of line","insert #{count} times at start of line"],
-          'a' => ["append after the cursor","append #{count} times after the cursor"],
-          'A' => ["append at end of line","append #{count} times at end of line"],
-          's' => ["delete current character and switch to insert mode","delete #{count} characters and switch to insert mode"],
-          'S' => ["delete current line and switch to insert mode"],
-          'S' => ["delete current line and switch to insert mode","delete #{count} lines and switch to insert mode"],
-          'o' => ["open a new line below the current line, switch to insert mode","#{count} times open a new line below the current line"],
-          'O' => ["open a new line above the current line, switch to insert mode"],
+          'i' => [
+            "insert in front of cursor",
+            "insert #{count} times in front of cursor"
+          ],
+          'I' => [
+            "insert at start of line",
+            "insert #{count} times at start of line"
+          ],
+          'a' => [
+            "append after the cursor",
+            "append #{count} times after the cursor"
+          ],
+          'A' => [
+            "append at end of line",
+            "append #{count} times at end of line"
+          ],
+          's' => [
+            "delete current character and switch to insert mode",
+            "delete #{count} characters and switch to insert mode"
+          ],
+          'S' => [
+            "delete current line and switch to insert mode",
+            "delete #{count} lines and switch to insert mode"
+          ],
+          'o' => [
+            "open a new line below the current line, switch to insert mode",
+            "#{count} times open a new line below the current line"
+          ],
+          'O' => [
+            "open a new line above the current line, switch to insert mode"
+          ],
           # misc
           'u' => ["undo the last change"],
         },
