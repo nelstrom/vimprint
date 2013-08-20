a878a3e1e49513b89e064b8407179631543d6576
Drew Neil <andrew.jr.neil@gmail.com>
2013-08-20 11:09:40 +0200

    Sketch 2 possible event models

--- lib/vimprint/model/modes.rb
+++ lib/vimprint/model/modes.rb
@@ -19,3 +19,55 @@ module Vimprint
   end
 
 end
+
+# Rationale:
+#       the `i` switch opens Normal mode
+#       from POV of Normal mode, the Insertion is one event, triggered by the switch
+# NormalMode[
+#   Motion.new('j'),
+#   Motion.new('k'),
+#   Motion.new('l'),
+#   InsertMode.new(Switch.new('i'), [
+#     Input.new('h'),
+#     Input.new('e'),
+#     Input.new('l'),
+#     Input.new('l'),
+#     Input.new('o'),
+#     ExpressionLineMode.new(Switch.new('<C-r>='), [
+#       Input.new('4'),
+#       Input.new('0'),
+#       Input.new('+'),
+#       Input.new('2'),
+#       Terminator.new("\n"),
+#     ]
+#     Terminator.new("\e"),
+#   ]),
+#   Motion.new('j'),
+# ]
+
+# Rationale:
+#       the `i` switch is a Normal mode command, so doesn't belong in Insert mode
+#       the switch event *owns* the Insertion, by association/proximity
+# NormalMode[
+#   Motion.new('j'),
+#   Motion.new('k'),
+#   Motion.new('l'),
+#   Switch.new('i'),
+#   InsertMode.new([
+#     Input.new('h'),
+#     Input.new('e'),
+#     Input.new('l'),
+#     Input.new('l'),
+#     Input.new('o'),
+#     Switch.new('<C-r>='),
+#     ExpressionLineMode[
+#       Input.new('4'),
+#       Input.new('0'),
+#       Input.new('+'),
+#       Input.new('2'),
+#       Terminator.new("\n"),
+#     ]
+#     Terminator.new("\e"),
+#   ]),
+#   Motion.new('j'),
+# ]
