d232af5441e83495fd33b4dc7f68b1825d5131b5
Drew Neil <andrew.jr.neil@gmail.com>
2013-08-26 20:16:07 +0100

    Refactor vimprint module

--- lib/vimprint.rb
+++ lib/vimprint.rb
@@ -4,20 +4,21 @@ require "vimprint/formatters/explainer"
 require "vimprint/ragel/parser"
 
 module Vimprint
+
   def self.explain(keystrokes)
-    eventlist = NormalMode.new
-    parser = Parser.new(eventlist)
-    parser.process(keystrokes)
-    explainer = Explainer.new(eventlist)
-    explainer.explain
+    Explainer.new(process(keystrokes)).explain
   end
 
   def self.pp(keystrokes)
-    eventlist = NormalMode.new
-    parser = Parser.new(eventlist)
-    parser.process(keystrokes)
-    printer = Printer.new(eventlist)
-    printer.print
+    Printer.new(process(keystrokes)).print
+  end
+
+  private
+
+  def self.process(keystrokes)
+    NormalMode.new.tap do |eventlist|
+      Parser.new(eventlist).process(keystrokes)
+    end
   end
 
 end
